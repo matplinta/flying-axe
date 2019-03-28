c2877260d572dcee67c546fc118d14d1b4a1ab81
Patryk Sladek <sladzio995@gmail.com>
2019-03-28 15:46:23 +0100

    *Refactored code

--- Assets/FlyingAxe/Scripts/RecallSystem.ts
+++ Assets/FlyingAxe/Scripts/RecallSystem.ts
@@ -5,34 +5,49 @@ namespace game {
 
         OnUpdate(): void {
             let dt = this.scheduler.deltaTime();
-            this.world.forEach([game.Recall, ut.Core2D.TransformLocalPosition, ut.Core2D.TransformNode, ut.Entity, ut.Core2D.TransformLocalRotation,ut.Core2D.TransformLocalScale],
-                (recall, transformLocalPosition, transformNode, entity, transformLocalRotation,transformLocalScale) => {
-                let callerEntity = recall.Caller;
-                let callerObjectToWorld = this.world.getComponentData(callerEntity, ut.Core2D.TransformObjectToWorld);
-                let callerWorldPos = new Vector3().setFromMatrixPosition(callerObjectToWorld.matrix);
-                let distance = transformLocalPosition.position.distanceTo(callerWorldPos);
-                if (distance < 1.0) {
-                    transformNode.parent = callerEntity;
-                    transformLocalPosition.position = new Vector3();
-                    transformLocalRotation.rotation = new Quaternion();
-                    transformLocalScale.scale = new Vector3(-4,4,1);
-                    this.world.removeComponent(entity, game.Recall);
-                    this.world.removeComponent(entity, game.Spin);
-                    this.world.removeComponent(entity, ut.Physics2D.RigidBody2D);
-                    let child = ut.Core2D.TransformService.getChild(this.world, entity, 0);
-                    this.world.usingComponentData(child, [ut.Core2D.TransformLocalRotation], localRotation => {
-                        localRotation.rotation = new Quaternion(0, 0, 0, 1);
+            this.world.forEach([game.Recall,
+                    ut.Core2D.TransformLocalPosition,
+                    ut.Core2D.TransformLocalRotation,
+                    ut.Core2D.TransformLocalScale,
+                    ut.Core2D.TransformNode,
+                    ut.Entity],
+                (recall, transformLocalPosition, transformLocalRotation, transformLocalScale, transformNode, recalledEntity) => {
+                    let callerEntity = recall.Caller;
+                    let callerObjectToWorld = this.world.getComponentData(callerEntity, ut.Core2D.TransformObjectToWorld);
+                    let callerWorldPos = new Vector3().setFromMatrixPosition(callerObjectToWorld.matrix);
+                    let distanceToCaller = transformLocalPosition.position.distanceTo(callerWorldPos);
+                    if (distanceToCaller < 1.0) {
+                        AttachToCaller(this.world, transformNode, transformLocalPosition, transformLocalRotation, transformLocalScale, callerEntity, recalledEntity);
+                    } else {
+                        MoveTowardsCaller(transformLocalPosition, callerWorldPos, recall);
+                    }
+                });
 
-                    });
-                    
-                    this.world.usingComponentData(callerEntity,[game.WeaponControl],weaponControl=>{
-                        weaponControl.isInHand = true;
-                    });
-                } else {
+            function MoveTowardsCaller(transformLocalPosition, callerWorldPos, recall) {
+                transformLocalPosition.position = new Vector3().lerpVectors(transformLocalPosition.position, callerWorldPos, dt * recall.speed);
+            }
 
-                    transformLocalPosition.position = new Vector3().lerpVectors(transformLocalPosition.position, callerWorldPos, dt * recall.speed);
-                }
-            });
+            function AttachToCaller(world, transformNode, transformLocalPosition, transformLocalRotation, transformLocalScale, callerEntity, recalledEntity) {
+                transformNode.parent = callerEntity;
+                transformLocalPosition.position = new Vector3();
+                transformLocalRotation.rotation = new Quaternion();
+                transformLocalScale.scale = new Vector3(-4, 4, 1);
+                world.removeComponent(recalledEntity, game.Recall);
+                world.removeComponent(recalledEntity, game.Spin);
+                world.removeComponent(recalledEntity, ut.Physics2D.RigidBody2D);
+                let entityChild = ut.Core2D.TransformService.getChild(world, recalledEntity, 0);
+                world.usingComponentData(entityChild, [ut.Core2D.TransformLocalRotation], localRotation => {
+                    localRotation.rotation = new Quaternion(0, 0, 0, 1);
+
+                });
+
+                world.usingComponentData(callerEntity, [game.WeaponControl], weaponControl => {
+                    weaponControl.isInHand = true;
+                });
+            }
+
         }
+
+
     }
 }
--- Assets/FlyingAxe/Scripts/WeaponService.ts
+++ Assets/FlyingAxe/Scripts/WeaponService.ts
@@ -1,37 +1,57 @@
 namespace game {
     export class WeaponService {
-        static ThrowWeapon(world: ut.World, axe: ut.Entity) {
-            world.usingComponentData(axe, [ut.Core2D.TransformLocalRotation, ut.Core2D.TransformLocalScale, ut.Core2D.TransformLocalPosition, ut.Core2D.TransformNode, ut.Core2D.TransformObjectToWorld],
-                (transformLocalRotation, transformLocalScale, transformLocalPosition, transformNode, objectToWorld) => {
-                    let worldPos = new Vector3().setFromMatrixPosition(objectToWorld.matrix);
-                    let worldRotation = ut.Core2D.TransformService.computeWorldRotation(world, axe);
-                    let worldScale = transformLocalScale.scale;
-                    world.usingComponentData(transformNode.parent,[ut.Core2D.TransformLocalScale],parentScale=>{
-                        worldScale = worldScale.multiply(parentScale.scale);
-                    });
-                    transformNode.parent = ut.NONE;
-                    transformLocalPosition.position = worldPos;
-                    transformLocalRotation.rotation = worldRotation;
-                    transformLocalScale.scale = worldScale;
-                    let right = new Vector3(1, 0, 0);
-                    right = right.applyQuaternion(worldRotation);
-                    let impulse = new ut.Physics2D.AddImpulse2D;
-                    impulse.impulse = new Vector2(right.x, right.y).multiplyScalar(15);
-                    let rb2d = new ut.Physics2D.RigidBody2D();
-                    rb2d.bodyType = ut.Physics2D.BodyType.BulletDynamic;
-                    rb2d.friction = 1;
-                    rb2d.freezeRotation = false;
-                    world.addComponentData(axe, rb2d);
-                    world.addComponentData(axe, impulse);
-                    if (!world.hasComponent(axe, game.Spin)) {
+        static ThrowWeapon(world: ut.World, weapon: ut.Entity) {
 
-                        let spin = new game.Spin();
-                        spin.speed = 40;
-                        spin.direction = right;
-                        world.addComponentData(axe, spin);
-
-                    }
+            world.usingComponentData(weapon,
+                [ut.Core2D.TransformLocalPosition,
+                    ut.Core2D.TransformLocalRotation,
+                    ut.Core2D.TransformLocalScale,
+                    ut.Core2D.TransformNode,
+                    ut.Core2D.TransformObjectToWorld],
+                (transformLocalPosition, transformLocalRotation, transformLocalScale, transformNode, objectToWorld) => {
+                    let right = DetachFromCaller(objectToWorld, transformLocalPosition, transformLocalRotation, transformLocalScale, transformNode);
+                    ApplyForceInDirection(right);
+                    StartSpinningTowards(right);
                 });
+
+            function DetachFromCaller(objectToWorld, transformLocalPosition, transformLocalRotation, transformLocalScale, transformNode) {
+                let worldPos = new Vector3().setFromMatrixPosition(objectToWorld.matrix);
+                let worldRotation = ut.Core2D.TransformService.computeWorldRotation(world, weapon);
+                let worldScale = transformLocalScale.scale;
+                world.usingComponentData(transformNode.parent, [ut.Core2D.TransformLocalScale], parentScale => {
+                    worldScale = worldScale.multiply(parentScale.scale);
+                });
+                transformNode.parent = ut.NONE;
+                transformLocalPosition.position = worldPos;
+                transformLocalRotation.rotation = worldRotation;
+                transformLocalScale.scale = worldScale;
+                let right = new Vector3(1, 0, 0);
+                right = right.applyQuaternion(worldRotation);
+                return right;
+            }
+
+
+            function ApplyForceInDirection(right) {
+                let impulse = new ut.Physics2D.AddImpulse2D;
+                impulse.impulse = new Vector2(right.x, right.y).multiplyScalar(15);
+                let rb2d = new ut.Physics2D.RigidBody2D();
+                rb2d.bodyType = ut.Physics2D.BodyType.BulletDynamic;
+                rb2d.friction = 1;
+                rb2d.freezeRotation = false;
+                world.addComponentData(weapon, rb2d);
+                world.addComponentData(weapon, impulse);
+            }
+
+            function StartSpinningTowards(right) {
+                if (!world.hasComponent(weapon, game.Spin)) {
+
+                    let spin = new game.Spin();
+                    spin.speed = 40;
+                    spin.direction = right;
+                    world.addComponentData(weapon, spin);
+
+                }
+            }
         }
 
         static RecallWeapon(world: ut.World, axe: ut.Entity, caller: ut.Entity) {
